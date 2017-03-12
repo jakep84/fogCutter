536e9c352ef0c0346b0f17d99b3c685022028284
Jake Priddy <priddyjacob84@gmail.com>
2017-03-12 18:16:32 -0400

    built AR cam scene

---
+++ fcHud/Assets/webCamScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class webCamScript : MonoBehaviour {
+
+	public GameObject webCameraPlane;
+
+	// Use this for initialization
+	void Start () {
+
+		if (Application.isMobilePlatform) {
+			GameObject cameraParent = new GameObject ("camParent");
+			cameraParent.transform.position = this.transform.position;
+			this.transform.parent = cameraParent.transform;
+			cameraParent.transform.Rotate (Vector3.right, 90);
+		}
+
+		Input.gyro.enabled = true;
+
+		WebCamTexture webCameraTexture = new WebCamTexture();
+		webCameraPlane.GetComponent<MeshRenderer> ().material.mainTexture = webCameraTexture;
+		webCameraTexture.Play ();
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		Quaternion cameraRotation = new Quaternion (Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+		this.transform.localRotation = cameraRotation;
+
+	}
+}
