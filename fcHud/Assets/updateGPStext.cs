ee3b0307674180f320fd165d6db503fb168bae0b
Jake Priddy <priddyjacob84@gmail.com>
2017-03-12 19:09:46 -0400

    added realtime GPS functionality

---
+++ fcHud/Assets/updateGPStext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateGPStext : MonoBehaviour 
+{
+	public Text coordinates;
+	private void Update()
+	{
+		coordinates.text = GPS.Instance.latitude.ToString () + "  |  " + GPS.Instance.longitude.ToString(); 
+	}
+
+}
